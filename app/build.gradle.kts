3c9f669995ca6989bc80f9da92b9fd0cd436a091
alaricbat <tatchu2001@gmail.com>
2025-07-18 08:33:45 +0700

    AI:SoftmaxRegression:180725:Update build gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.jobfix"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.jobfix"
         minSdk = 33
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
