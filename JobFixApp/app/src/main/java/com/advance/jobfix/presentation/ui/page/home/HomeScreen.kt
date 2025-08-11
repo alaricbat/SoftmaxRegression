e8fdff3a6c288ad6b69b20888fec4f27af2997a7
alaricbat <tatchu2001@gmail.com>
2025-08-11 22:18:46 +0700

    Commit:SoftmaxRegression:11082025:First phase for building HomeScreen.

--- JobFixApp/app/src/main/java/com/advance/jobfix/presentation/ui/page/home/HomeScreen.kt
+++ JobFixApp/app/src/main/java/com/advance/jobfix/presentation/ui/page/home/HomeScreen.kt
@@ -1,36 +1,143 @@
 package com.advance.jobfix.presentation.ui.page.home
 
+import android.text.InputType
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.TopAppBarState
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.advance.jobfix.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    openDrawer: () -> Unit,
+//    snackbarHostState: SnackbarHostState,
+    modifier: Modifier = Modifier
+) {
 
-    Row (
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxSize(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
+    val topAppBarState = rememberTopAppBarState()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(topAppBarState)
+
+    Scaffold (
+        snackbarHost = {
+        },
+        topBar = {
+            HomeTopAppBar(
+                openDrawer = openDrawer,
+                topAppBarState = topAppBarState
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+
+        Column(
             modifier = Modifier
-                .fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            text = "Home Screen",
-        )
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = stringResource(R.string.str_app_description).trimIndent(),
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(10.dp)
+            )
+
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    textAlign = TextAlign.Center,
+                    text = stringResource(R.string.str_linguistic) + " : ",
+                    fontWeight = FontWeight.Bold
+                )
+
+                SimpleTextField()
+
+            }
+
+        }
+
     }
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun HomeTopAppBar(
+    openDrawer: () -> Unit,
+    modifier: Modifier = Modifier,
+    topAppBarState: TopAppBarState = rememberTopAppBarState(),
+    scrollBehavior: TopAppBarScrollBehavior? =
+        TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
+) {
+    val context = LocalContext.current
+    val title = stringResource(id = R.string.app_name)
+    CenterAlignedTopAppBar(
+        title = {
+            Image(
+                painter = painterResource(R.drawable.home_title),
+                contentDescription = title,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    )
+}
+
+@Composable
+fun SimpleTextField() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = { newText ->
+            text = newText
+        }
+    )
+}
+
+
 @Preview("Home screen")
 @Composable
 fun PreviewHomeScreen() {
-    HomeScreen()
+    HomeScreen(
+        openDrawer = {},
+    )
 }
