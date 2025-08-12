044cec4bf705d0d19d65c0f026cccc5ff9bae391
alaricbat <tatchu2001@gmail.com>
2025-08-12 20:46:15 +0700

    Commit:SoftmaxRegression:12082025:Third phase for designing HomeScreen - without applying [PATTERN DESIGN].

--- JobFixApp/app/src/main/java/com/advance/jobfix/presentation/ui/page/home/HomeScreen.kt
+++ JobFixApp/app/src/main/java/com/advance/jobfix/presentation/ui/page/home/HomeScreen.kt
@@ -1,23 +1,24 @@
 package com.advance.jobfix.presentation.ui.page.home
 
-import android.text.InputType
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
@@ -25,17 +26,18 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.TopAppBarState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,9 +49,11 @@ import com.advance.jobfix.R
 fun HomeScreen(
     openDrawer: () -> Unit,
 //    snackbarHostState: SnackbarHostState,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+//    state: LazyListState = rememberLazyListState()
 ) {
 
+    val focusManager = LocalFocusManager.current
     val topAppBarState = rememberTopAppBarState()
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(topAppBarState)
 
@@ -59,84 +63,118 @@ fun HomeScreen(
         topBar = {
             HomeTopAppBar(
                 openDrawer = openDrawer,
-                topAppBarState = topAppBarState
+                topAppBarState = topAppBarState,
+                scrollBehavior = scrollBehavior
             )
         },
         modifier = modifier
     ) { innerPadding ->
 
-        Column(
-            modifier = Modifier
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(20.dp),
+        val contentModifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+
+        LazyColumn (
+            modifier = contentModifier
+                .padding(innerPadding)
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                ) {
+                    focusManager.clearFocus()
+                },
+            verticalArrangement = Arrangement.spacedBy(10.dp),
         ) {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = stringResource(R.string.str_app_description).trimIndent(),
-            )
 
-            Spacer(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(10.dp)
-            )
+            val linguisticText = mutableStateOf(TextFieldValue(""))
+            val musicalText = mutableStateOf(TextFieldValue(""))
+            val bodilyText = mutableStateOf(TextFieldValue(""))
 
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(PaddingValues(10.dp)),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
+            item {
                 Text(
-                    modifier = Modifier.width(100.dp),
-                    textAlign = TextAlign.Start,
-                    text = stringResource(R.string.str_linguistic) + " : ",
-                    fontWeight = FontWeight.Bold
+                    modifier = Modifier.padding(8.dp),
+                    text = stringResource(R.string.str_app_description).trimIndent(),
                 )
-                SimpleTextField()
             }
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(PaddingValues(10.dp)),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    modifier = Modifier.width(100.dp),
-                    textAlign = TextAlign.Start,
-                    text = stringResource(R.string.str_musical).trimIndent() + " : ",
-                    fontWeight = FontWeight.Bold
+            item {
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(10.dp)
                 )
-                SimpleTextField()
             }
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(PaddingValues(10.dp)),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    modifier = Modifier.width(100.dp),
-                    textAlign = TextAlign.Start,
-                    text = stringResource(R.string.str_bodily).trimIndent(),
-                    fontWeight = FontWeight.Bold
-                )
-                SimpleTextField()
+            item {
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(PaddingValues(10.dp)),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        modifier = Modifier.width(100.dp),
+                        textAlign = TextAlign.Start,
+                        text = stringResource(R.string.str_linguistic) + " : ",
+                        fontWeight = FontWeight.Bold
+                    )
+                    RankTextField(linguisticText)
+                }
             }
 
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(PaddingValues(50.dp)),
-                onClick = {}
-            ) {
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = stringResource(R.string.str_get_result).trimIndent(),
-                    textAlign = TextAlign.Center
-                )
+            item {
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(PaddingValues(10.dp)),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        modifier = Modifier.width(100.dp),
+                        textAlign = TextAlign.Start,
+                        text = stringResource(R.string.str_musical).trimIndent() + " : ",
+                        fontWeight = FontWeight.Bold
+                    )
+                    RankTextField(musicalText)
+                }
+            }
+
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(PaddingValues(10.dp)),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        modifier = Modifier.width(100.dp),
+                        textAlign = TextAlign.Start,
+                        text = stringResource(R.string.str_bodily).trimIndent(),
+                        fontWeight = FontWeight.Bold
+                    )
+                    RankTextField(bodilyText)
+                }
+
+            }
+
+            item {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(PaddingValues(50.dp)),
+                    onClick = {
+                        val totalRank =
+                            linguisticText.value.text + " - " +
+                                    musicalText.value.text + " - " +
+                                    bodilyText.value.text
+                        Log.d("HomeScreen", totalRank)
+                    }
+                ) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = stringResource(R.string.str_get_result).trimIndent(),
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
 
         }
@@ -154,7 +192,6 @@ private fun HomeTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior? =
         TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
 ) {
-    val context = LocalContext.current
     val title = stringResource(id = R.string.app_name)
     CenterAlignedTopAppBar(
         title = {
@@ -163,20 +200,25 @@ private fun HomeTopAppBar(
                 contentDescription = title,
                 modifier = Modifier.fillMaxWidth()
             )
-        }
+        },
+        scrollBehavior = scrollBehavior
     )
 }
 
 @Composable
-fun SimpleTextField() {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
+fun RankTextField(value: MutableState<TextFieldValue>) {
+
     TextField(
         modifier = Modifier
             .fillMaxWidth()
             .padding(start = 10.dp),
-        value = text,
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number
+        ),
+        value = value.value,
         onValueChange = { newText ->
-            text = newText
+            value.value = newText
         }
     )
 }
